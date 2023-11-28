5d846ecab2cc223cddf7e3705eaf95ce075a9308
Santiago MB <santiagomnoz318@gmail.com>
2023-11-27 22:56:39 -0500

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,14 @@
-import Head from 'next/head'
-import styles from "./Index.module.css"
-import { Typography } from '@mui/material'
-import { ImgComparisonSlider } from '@img-comparison-slider/react'
 import Head from "next/head";
+import styles from "./Index.module.css";
+import { Typography } from "@mui/material";
+import { ImgComparisonSlider } from "@img-comparison-slider/react";
 import { Inter } from "next/font/google";
 import { MainPage } from "@/components/MainPage";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-
-
   const goToSection = (href) => {
-
     const target = document.getElementById(href);
     if (target) {
       const targetOffset = target.offsetTop;
@@ -22,7 +18,6 @@ export default function Home() {
         behavior: "smooth",
       });
     }
-
   };
 
   return (
@@ -50,25 +45,33 @@ export default function Home() {
           </div>
           <div className={styles.foptions}>
             <div className={styles.boptions}>
-              <a color={'white'} href='#homeSection' onClick={goToSection}>Inicio</a>
+              <a color={"white"} href="#homeSection" onClick={goToSection}>
+                Inicio
+              </a>
             </div>
             <div className={styles.boptions}>
-              <a color={'white'} href='#procSection' onClick={goToSection}>Procedimientos</a>
+              <a color={"white"} href="#procSection" onClick={goToSection}>
+                Procedimientos
+              </a>
             </div>
             <div className={styles.boptions}>
-              <a color={'white'} href='#auSection' onClick={goToSection}>Acerca de nosotros</a>
+              <a color={"white"} href="#auSection" onClick={goToSection}>
+                Acerca de nosotros
+              </a>
             </div>
             <div className={styles.boptions}>
-              <a color={'white'} href='#casesSection' onClick={goToSection}>Casos de éxito</a>
+              <a color={"white"} href="#casesSection" onClick={goToSection}>
+                Casos de éxito
+              </a>
             </div>
           </div>
         </div>
-        <div id='homeSection' className={styles.bsection}>
+        <div id="homeSection" className={styles.bsection}>
           <div className={styles.boxexam}></div>
         </div>
-        <div id='procSection' className={styles.bsection}></div>
-        <div id='auSection' className={styles.bsection}></div>
-        <div id='casesSection' className={styles.ausection}>
+        <div id="procSection" className={styles.bsection}></div>
+        <div id="auSection" className={styles.bsection}></div>
+        <div id="casesSection" className={styles.ausection}>
           <div className={styles.bslider}>
             <ImgComparisonSlider>
               <div slot="first" className={styles.box1}></div>
