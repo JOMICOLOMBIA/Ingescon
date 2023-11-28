6bc1da041b9a034253276cb3e5377b39662d92c5
xOpusMagnumx <zambrano.meza901@gmail.com>
2023-11-27 19:48:21 -0500

    Added section redirection, just the logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+const defaultTheme = createTheme({
+  components: {
+    MuiLink: {
+      defaultProps: {
+        underline: 'none',
+      },
+    },
+  }
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+  return (
+    <ThemeProvider theme={defaultTheme} >
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+};
