9fa0a4ed34d09e897a987aaafe41d00e096b8d7a
Santiago MB <santiagomnoz318@gmail.com>
2023-11-28 21:36:54 -0500

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
-import styles from "./Index.module.css";
-import { Typography } from "@mui/material";
-import { ImgComparisonSlider } from "@img-comparison-slider/react";
+
 import { MainPage } from "@/components/MainPage";
 
 export default function Home() {
@@ -27,54 +25,7 @@ export default function Home() {
         
       </Head>
       <main>
-        <div className={styles.navbar}>
-          <div className={styles.blogo}>
-            <Typography>Hola.</Typography>
-          </div>
-          <div className={styles.foptions}>
-            <div className={styles.boptions}>
-              <a color={"white"} href="#homeSection" onClick={goToSection}>
-                Inicio
-              </a>
-            </div>
-            <div className={styles.boptions}>
-              <a color={"white"} href="#procSection" onClick={goToSection}>
-                Procedimientos
-              </a>
-            </div>
-            <div className={styles.boptions}>
-              <a color={"white"} href="#auSection" onClick={goToSection}>
-                Acerca de nosotros
-              </a>
-            </div>
-            <div className={styles.boptions}>
-              <a color={"white"} href="#casesSection" onClick={goToSection}>
-                Casos de éxito
-              </a>
-            </div>
-          </div>
-        </div>
-        <div id="homeSection" className={styles.bsection}>
-          <div className={styles.boxexam}></div>
-        </div>
-        <div id="procSection" className={styles.bsection}></div>
-        <div id="auSection" className={styles.bsection}></div>
-        <div id="casesSection" className={styles.ausection}>
-          <div className={styles.bslider}>
-            <ImgComparisonSlider>
-              <div slot="first" className={styles.box1}></div>
-              <div slot="second" className={styles.box2}></div>
-            </ImgComparisonSlider>
-            <ImgComparisonSlider>
-              <div slot="first" className={styles.box1}></div>
-              <div slot="second" className={styles.box2}></div>
-            </ImgComparisonSlider>
-            <ImgComparisonSlider>
-              <div slot="first" className={styles.box1}></div>
-              <div slot="second" className={styles.box2}></div>
-            </ImgComparisonSlider>
-          </div>
-        </div>
+        
         <MainPage />
       </main>
     </>
