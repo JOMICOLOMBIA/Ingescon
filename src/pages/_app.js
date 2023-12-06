5b3227710b52e201dfaa958d0be89836c6fdfe5f
Santiago MB <santiagomnoz318@gmail.com>
2023-12-05 21:06:06 -0500

    .2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,6 @@ import "../styles/global.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Navbar } from "@/components/Layout/Navbar";
 import { Footer } from "@/components/Layout/Footer";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }) {
   return (
