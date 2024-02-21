1483160b28c46eb9058851cf77ae3fb5ed507fd2
xOpusMagnumx <zambrano.meza901@gmail.com>
2024-02-21 14:43:41 -0500

    .

---
+++ src/components/Layout/HomeCarouselContent/index.js
@@ -0,0 +1,45 @@
+import { FlexHomeSection } from "../FlexHomeSection"
+import Image from "next/image"
+import { useMediaQuery } from "@mui/material"
+import styles from "./index.module.css"
+
+const homeContent = [
+    {
+        title: "",
+        desc: "",
+    },
+    {
+        title: "",
+        desc: "",
+    },
+    {
+        title: "",
+        desc: "",
+    },
+
+]
+
+
+export const HomeCarouselContent = () => {
+
+    const screenUpper576 = useMediaQuery("(min-width:576px)");
+
+
+    return (
+        <>
+            <Image
+                src={
+                    screenUpper576
+                        ? "/assets/home-slider-1.jpg"
+                        : "/assets/Enmascarar_grupo_73.jpg"
+                } // Ruta relativa a la imagen dentro de la carpeta public
+                alt="Mujer sonriendo"
+                width={100} // Ancho de la imagen en píxeles
+                height={100} // Alto de la imagen en píxeles
+                layout="responsive"
+                className={styles.image}
+            />
+            <FlexHomeSection />
+        </>
+    )
+}
