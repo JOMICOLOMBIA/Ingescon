8fd1c42398f723e21bab430bb65b5f7d7d120157
xOpusMagnumx <zambrano.meza901@gmail.com>
2024-03-03 12:48:03 -0500

    .

--- src/components/Layout/HomeCarouselContent/index.js
+++ src/components/Layout/HomeCarouselContent/index.js
@@ -10,28 +10,23 @@ import { useEffect } from "react"
 export const HomeCarouselContent = ({ pic }) => {
 
     const screenUpper576 = useMediaQuery("(min-width:576px)");
-
-    // function bgImage() {
-    //     const element = document.getElementById("myDIV");
-    //     element.classList.add("background-image: url('/assets/logowhite.png')");
-    // }
-
-    // useEffect(() => {
-    //     bgImage()
-    // }, []);
-
+    // style={{ backgroundImage: screenUpper576 ? `url(/assets/home-slider-${pic}.webp)` : `url(/assets/test-1.jpg)`  }}
 
     return (
         <div className={styles.bsectionHome}>
-            <div id="carouselImageBox" className={styles.homeImageBox} style={{ backgroundImage: `url(/assets/home-slider-${pic}.webp)` }}>
-            {/* <Image
-                src={`/assets/home-slider-${pic}.webp`} // Ruta relativa a la imagen dentro de la carpeta public
+            <div id="carouselImageBox" className={styles.homeImageBox} >
+            <Image
+                src={
+                    screenUpper576
+                      ? `/assets/home-slider-${pic}.webp`
+                      : `/assets/test-${pic}.jpg`
+                  }
                 alt="Fondo"
                 fill={true}
                 className={styles.image}
                 sizes="(max-width: 728px) 100vw, (max-width: 2000px) 100vw"
                 priority={true}
-            /> */}
+            />
             </div>
             <FlexHomeSection pic={pic} />
 
