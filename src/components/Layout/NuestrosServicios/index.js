655fb41b0adf40eac324b4444efcf7895aef9081
xOpusMagnumx <zambrano.meza901@gmail.com>
2024-03-04 12:58:53 -0500

    .

--- src/components/Layout/NuestrosServicios/index.js
+++ src/components/Layout/NuestrosServicios/index.js
@@ -103,7 +103,6 @@ export const NuestrosServicios = () => {
                             />
                         );
                     }}
-
                 >
                     {
                         contentCard.map((content, key) => {
