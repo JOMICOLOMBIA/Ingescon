c46bf428b294923f52dcf29c105a139c8c1c5ecb
xOpusMagnumx <zambrano.meza901@gmail.com>
2024-03-08 17:36:03 -0500

    .

--- src/components/Layout/ContentFlexProyectos/index.js
+++ src/components/Layout/ContentFlexProyectos/index.js
@@ -8,24 +8,26 @@ export const ContentFlexProyectos = ({key, proyect}) => {
 
     const screenUpper576 = useMediaQuery(("max-width: 576px"));
     const [isClicked, setIsClicked] = useState(false);
+    const box = document.getElementById("topBox");
+    // const [topBoxClass, setTopBoxClass] = useState(styles.topBoxNotClicked)
 
 
     const handleTap = () => {
+        
         setIsClicked(!isClicked);
+
+        // if (isClicked === false)
     }
 
 
     return (
-        <div className={styles.boxHoverEffect} onClick={handleTap} key={key}>
+        <div className={styles.boxHoverEffect} onClick={() => handleTap()} key={key}>
             <div className={styles.bottomBox}>
                 <Image // src="/assets/proyect-1.png"
                     src={`/assets/proyect-${proyect.image}.png`} alt="Foto proyecto" fill={true} className={styles.imageProject} />
             </div>
 
-            <div className={styles.topBox} id="topBox" style={isClicked && screenUpper576 ? {
-                opacity: 1,
-                transition: "opacity 0.2s ease-in-out"
-            } : {}}>
+            <div className={isClicked ? styles.topBoxClicked : styles.topBoxNotClicked} id="topBox">
                 <Image // src="/assets/proyect-1.png"
                     src={"/assets/hover-view.png"} alt="cuadro negro con alfa baja" fill={true} className={styles.imageHover} />
                 <h3 className={styles.titleCardProyect}>{proyect.title}</h3>
